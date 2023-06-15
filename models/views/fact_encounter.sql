3bad53e4871abf5095ebb351fd3488c0d35bd40e
Marat Surmashev <muradbei@gmail.com>
2023-06-15 18:55:19 +0200

    Fix

--- models/views/fact_encounter.sql
+++ models/views/fact_encounter.sql
@@ -1,4 +1,14 @@
+{{
+     config(
+          materialized='incremental',
+          unique_key='id',
+          indexes=[ {'columns': ['id']},
+                    {'columns': ['ts']}]
+    )
+}}
+
 SELECT id
+       , ts
        , {{ aidbox.path('status')}} status
        , {{ aidbox.codesystem_code('type', 'SNOMED CT-INT')}} type_code
        , {{ aidbox.codesystem_display('type', 'SNOMED CT-INT')}} type_display
@@ -11,4 +21,8 @@ SELECT id
        , {{ aidbox.identifier_from_uri('location,0,location,uri')}} location_synthea_id
        , {{ aidbox.identifier_from_uri('serviceProvider,uri')}} organization_synthea_id
        , {{ aidbox.identifier_from_uri('participant,0,individual,uri')}} practitioner_npi
-  FROM {{ ref('Encounter') }}
+  FROM {{ ref('aidbox', 'Encounter') }}
+
+{% if is_incremental() %}
+  WHERE ts > (select max(ts) from {{ this }})
+{% endif %}
